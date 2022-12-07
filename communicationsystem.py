bf1ebd084f0eef9e9c81d22907e02f2bdd556368
HyeonGeonYu <108247128+HyeonGeonYu@users.noreply.github.com>
2022-12-07 11:04:16 +0900

    과제용으로 변경

--- communicationsystem.py
+++ communicationsystem.py
@@ -73,10 +73,22 @@ def modulation(inp_class):
     if inp_class.modulation_scheme == "BPSK":
         inp_class.modulation_result = np.where(inp_class.channel_coding_result_np  == 2, np.nan, inp_class.channel_coding_result_np)
         inp_class.modulation_result = np.where(inp_class.modulation_result == 0, -1, inp_class.modulation_result)
+    elif inp_class.modulation_scheme == "QPSK":
+        refer_arr  = inp_class.channel_coding_result_np.reshape(-1,2)
+        inp_class.modulation_result =np.zeros(refer_arr.shape[0],dtype='complex')
+        inp_class.modulation_result = np.where((refer_arr == [0, 0]).all(axis=1),1+1j,inp_class.modulation_result)
+        inp_class.modulation_result = np.where((refer_arr == [1, 0]).all(axis=1),-1+1j,inp_class.modulation_result)
+        inp_class.modulation_result = np.where((refer_arr == [0, 1]).all(axis=1),1-1j,inp_class.modulation_result)
+        inp_class.modulation_result = np.where((refer_arr == [1, 1]).all(axis=1),-1-1j,inp_class.modulation_result)
     else:
         raise Exception('모듈레이션 scheme 확인필요')
 def channel_awgn(inp_class):
-    inp_class.channel_result = inp_class.modulation_result + np.random.normal(inp_class.mu, inp_class.std, inp_class.modulation_result.shape)
+    if inp_class.modulation_scheme == "BPSK":
+        inp_class.channel_result = inp_class.modulation_result + np.random.normal(inp_class.mu, inp_class.std, inp_class.modulation_result.shape)
+    elif inp_class.modulation_scheme == "QPSK":
+        mod_size = inp_class.modulation_result.size
+        mod_shape = inp_class.modulation_result.shape
+        inp_class.channel_result = inp_class.modulation_result + np.random.normal(inp_class.mu, inp_class.std, (mod_size, 2)).view(np.complex).reshape(mod_shape)
 def demodulation(inp_class):
     if inp_class.modulation_scheme == "BPSK":
         inp_class.demodulation_result = np.where(inp_class.channel_result < 0, 0,
@@ -84,6 +96,17 @@ def demodulation(inp_class):
         inp_class.demodulation_result = np.where(inp_class.demodulation_result  > 0, 1,
                                                inp_class.demodulation_result)
         inp_class.demodulation_result = np.where(np.isnan(inp_class.demodulation_result),2,inp_class.demodulation_result).astype('uint8')
+    elif inp_class.modulation_scheme == "QPSK":
+        inp_class.demodulation_result = np.zeros_like(inp_class.channel_coding_result_np).reshape(-1,2)
+        real_arr = inp_class.channel_result.real
+        imag_arr = inp_class.channel_result.imag
+        inp_class.demodulation_result[np.where((real_arr > 0) & (imag_arr > 0))] = np.array([0,0])
+        inp_class.demodulation_result[np.where((real_arr < 0) & (imag_arr > 0))] = np.array([1,0])
+        inp_class.demodulation_result[np.where((real_arr > 0) & (imag_arr < 0))] = np.array([0,1])
+        inp_class.demodulation_result[np.where((real_arr < 0) & (imag_arr < 0))] = np.array([1,1])
+        inp_class.demodulation_result = inp_class.demodulation_result.reshape(inp_class.channel_coding_result_np.shape)
+
+
 
     else:
         raise Exception('모듈레이션 scheme 확인필요')
@@ -166,21 +189,17 @@ def source_decoder(inp_class) :
                 inp_class.inp_data.shape)
 
 def make_result_class(inp_file_dir,source_coding_type,channel_coding_type,draw_huffmantree,modulation_scheme,mu,std):
-    inp_data, mapped_data, inp_data_unique_arr,inp_data_unique_arr_idx_arr, count, inp_bit_len, ext = read_file_func(inp_file_dir)
+    inp_data, mapped_data, inp_data_unique_arr,inp_data_unique_arr_idx_arr, count, inp_bit_len, ext = None, None, None, None, None, None, None
 
     inp_class = communicationsystem(ext, inp_data, mapped_data,inp_data_unique_arr, inp_data_unique_arr_idx_arr,count,
                                     source_coding_type,channel_coding_type, inp_bit_len,draw_huffmantree,
                                     modulation_scheme,
                                     mu,std)
 
-    source_encoder(inp_class)
-    channel_coding(inp_class)
+    inp_class.channel_coding_result_np = np.random.randint(0,2,(100,16)) #0과1 랜덤하게 1600개 생성
+
     modulation(inp_class)
-
     channel_awgn(inp_class)
-
     demodulation(inp_class)
-    channel_decoding(inp_class)
-    source_decoder(inp_class)
 
     return inp_class
--- main_simul.py
+++ main_simul.py
@@ -2,57 +2,32 @@
 import numpy as np
 import communicationsystem
 import between_std_SNR
-import cv2
+import matplotlib.pyplot as plt
 
-#inp_file_dir = 'sample.txt'
-inp_file_dir = 'Lenna.png'
-source_coding_type = "Huffman"
-#source_coding_type = "NoCompression"
-channel_coding_type = "Repetition"
-#channel_coding_type = "NoChannelCoding"
 
-draw_huffmantree = False      # huffman이 아니면 True여도 안그림.
-modulation_scheme = "BPSK"
+inp_file_dir = None
+source_coding_type = None
+channel_coding_type = None
+draw_huffmantree = None
+modulation_scheme = "QPSK"
 mu = 0
-SNR = 1 #dB
-std = between_std_SNR.SNR_2_std(SNR)
 
-result_class = communicationsystem.make_result_class(inp_file_dir,source_coding_type,channel_coding_type,draw_huffmantree,
-                                               modulation_scheme, mu, std)
-#####압축률 확인
-result_class.source_coding_result_bit_num / result_class.mapped_data_bit_num
-'''
-    수학적 압축률(엔트로피 / mapped_data_bit_len) == 허프만 코딩의 바운드임(더 좋을 수 없다.)
-    prob_arr = result_class.count/np.sum(result_class.count)
-    -np.sum(prob_arr * np.log2(prob_arr))/ prob_arr.size.bit_length()
-'''
-#####
-
-
-#####BER 확인
-source_coding_result_bit_num = result_class.source_coding_result_bit_num #보내는 비트수
-channel_coding_result_bit_num = result_class.channel_coding_result_bit_num
-
-#원래 BER
-err_bit_num = np.sum(result_class.channel_coding_result_np != result_class.demodulation_result)#에러난 비트수
-BER = err_bit_num/channel_coding_result_bit_num
-print("%.2fdB, %.4f "%(SNR, BER))
-
-#채널 코딩으로 복원되서 더 좋아진 BER
-err_bit_num =np.sum(result_class.source_coding_result_np !=result_class.channel_decoding_result_np) #에러난 비트수
-recovered_BER = err_bit_num/source_coding_result_bit_num
-print("%.2fdB, %.4f "%(SNR, recovered_BER))
-'''
-BER, recovered_BER, math_BER, math_BER_with_channel_coding_3_rept
-수학적 BPSK의  BER => 평균적인 결과라서 바운드가 아님 (더 좋을 수 있다.)
-from scipy import special
-math_BER = 0.5 - 0.5*special.erf(1/(std*np.sqrt(2)))
-math_BER_with_channel_coding_3_rept = 3*math_BER**2-2*math_BER**3 
-'''
-#####
-
-#result_class.inp_data, result_class.out_data
-#cv2.imwrite('Test_dir/Test1.png', result_class.inp_data)
-#cv2.imwrite('Test_dir/Test2.png', result_class.out_data)
-pass
-
+SNR_arr = np.arange(0,35,5)
+SER_result = []
+for SNR in SNR_arr: # SNR  #dB
+    std = between_std_SNR.SNR_2_std(SNR)
+    result_class = communicationsystem.make_result_class(inp_file_dir,source_coding_type,channel_coding_type,draw_huffmantree,
+                                                   modulation_scheme, mu, std)
+    #####SER 확인
+    Tx_sym = result_class.channel_coding_result_np.reshape(-1,2) #보낸 심볼
+    Rx_sym = result_class.demodulation_result.reshape(-1,2) # 받은 심볼
+    num_sym = Tx_sym.shape[0]
+    num_err = num_sym - np.count_nonzero(np.all(Tx_sym == Rx_sym,axis=1)) #err 갯수
+    SER = num_err/num_sym
+    SER_result.append(SER)
+    print("%.2fdB, %.4f "%(SNR, SER))
+plt.yscale('log', base=10)
+plt.ylim([10**-4,10**-0 ])
+plt.grid(linestyle = ':')
+plt.plot(SNR_arr,SER_result)
+plt.show()
